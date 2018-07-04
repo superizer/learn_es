87cfa930bd148a35c8e654dcb1dd87168fa4f2fa
superizer <superizer.tone@gmail.com>
2018-07-04 15:06:43 +0700

    fix random and and hill climbing

--- cart_pole_hill.py
+++ cart_pole_hill.py
@@ -4,15 +4,18 @@ import numpy as np
 env = gym.make('CartPole-v0')
 
 max_reward = 0
-max_parameters = 0
+max_parameters = []
+
+noise_scaling = 0.1
+parameters = np.random.rand(4) * 2 - 1
 
 for i_episode in range(2000):
     observation = env.reset()
     r = 0
-    parameters = np.random.rand(4) * 2 - 1
+    newparams = parameters + (np.random.rand(4) * 2 - 1)*noise_scaling
     for t in range(100):
         env.render()
-        #print(observation)
+        print(observation)
         #action = env.action_space.sample()
         action = 0 if np.matmul(parameters,observation) < 0 else 1
         observation, reward, done, info = env.step(action)
@@ -24,6 +27,7 @@ for i_episode in range(2000):
     if r > max_reward:
         max_reward = r
         max_parameters = parameters
+        parameters = newparams
 
 print('max reward = ', max_reward, ' max parameter = ', max_parameters)
 
